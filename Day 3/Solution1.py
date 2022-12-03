4a803eede7da35cec8f0f0562a887537f703376c
joll05 <joelolovnykanen@gmail.com>
2022-12-03 12:21:56 +0100

    Day 3

---
+++ Day 3/Solution1.py
@@ -0,0 +1,22 @@
+f = open("input.txt")
+input_raw = f.read().strip()
+f.close()
+input_lines = input_raw.split("\n")
+
+priority_sum = 0
+
+for rucksack in input_lines:
+    divider = len(rucksack) // 2
+    compartment_1, compartment_2 = set(rucksack[divider:]), set(rucksack[:divider])
+    intersection = compartment_1.intersection(compartment_2)
+
+    for item in intersection:
+        priority = ord(item.upper()) - ord('A') + 1
+
+        if(item.isupper()):
+            priority += 26
+        
+
+        priority_sum += priority
+
+print(priority_sum)
---
+++ Day 3/Solution2.py
@@ -0,0 +1,26 @@
+f = open("input.txt")
+input_raw = f.read().strip()
+f.close()
+input_lines = input_raw.split("\n")
+
+priority_sum = 0
+
+for group_number in range(len(input_lines) // 3):
+    group = input_lines[group_number * 3: group_number * 3 + 3]
+
+    intersection = set(group[0])
+
+    for rucksack in group[1:]:
+        intersection.intersection_update(rucksack)
+
+    # list unpacking to find only element in set
+    (item,) = intersection
+
+    priority = ord(item.upper()) - ord('A') + 1
+
+    if(item.isupper()):
+        priority += 26    
+
+    priority_sum += priority
+
+print(priority_sum)
