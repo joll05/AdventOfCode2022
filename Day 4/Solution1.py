3cc0d8ca6c3e809abd16f035de6bbf97b3ef5bb2
joll05 <joelolovnykanen@gmail.com>
2022-12-04 16:17:38 +0100

    Day 4

---
+++ Day 4/Solution1.py
@@ -0,0 +1,20 @@
+import re
+
+f = open("input.txt")
+input_raw = f.read().strip()
+f.close()
+input_lines = input_raw.splitlines()
+
+pattern = r'^(\d+)-(\d+),(\d+)-(\d+)$'
+
+overlaps = 0
+
+for pair in input_lines:
+    match = re.match(pattern, pair)
+    min1, max1 = int(match.group(1)), int(match.group(2))
+    min2, max2 = int(match.group(3)), int(match.group(4))
+
+    if (min1 >= min2 and max1 <= max2) or (min2 >= min1 and max2 <= max1):
+        overlaps += 1
+
+print(overlaps)
