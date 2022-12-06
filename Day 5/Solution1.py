6a03139031c9d506a1949f4f7b62b5ec4ef9d260
joll05 <joelolovnykanen@gmail.com>
2022-12-06 10:42:16 +0100

    Day 5

---
+++ Day 5/Solution1.py
@@ -0,0 +1,39 @@
+import re
+
+f = open("input.txt")
+input_raw = f.read()
+f.close()
+input_crates, input_instructions = input_raw.split("\n\n")
+crates_lines = input_crates.splitlines()[:-1]
+instruction_lines = input_instructions.strip().splitlines()
+
+stacks = []
+
+# if you're trying to understand how this works... fuck you, I'm not gonna bother commenting it
+for i in range(1, len(crates_lines[0]), 4):
+    stacks.append([])
+    for line in crates_lines[::-1]:
+        char = line[i]
+
+        if char == ' ':
+            break
+
+        stacks[-1].append(char)
+
+INSTRUCTION_PATTERN = r"^move (\d+) from (\d+) to (\d+)$"
+
+for instruction in instruction_lines:
+    match = re.match(INSTRUCTION_PATTERN, instruction)
+
+    count = int(match.group(1))
+    origin = int(match.group(2)) - 1
+    destination = int(match.group(3)) - 1
+
+    for i in range(count):
+        stacks[destination] += [stacks[origin][-1]]
+        stacks[origin] = stacks[origin][:-1]
+
+for stack in stacks:    
+    print(stack[-1], end="")
+
+print()    
